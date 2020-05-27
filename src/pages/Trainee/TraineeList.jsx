868b0f293d0a3278d27ad681a8b902fa263094cd
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-05-27 14:44:11 +0530

    feat: Add Sorting

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import propTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
 import { Link } from 'react-router-dom';
-import { FormDialog, TableComponent } from './components/index';
+import * as moment from 'moment';
+import { FormDialog, TableComponent } from './components';
 import trainee from './data/trainee';
 
 const useStyles = {
@@ -18,66 +20,99 @@ class TraineeList extends React.Component {
     super(props);
     this.state = {
       open: false,
+      order: 'asc',
+      orderBy: 'Date',
     };
   }
 
-   handleClickOpen = () => {
-     this.setState({ open: true });
-   };
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
 
-   handleClose = () => {
-     this.setState({ open: false });
-   };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
-   onSubmitHandle = (values) => {
-     this.setState({ open: false });
-     console.log(values);
-   }
+  onSubmitHandle = (values) => {
+    this.setState({ open: false });
+    console.log(values);
+  }
 
-   render() {
-     const { open } = this.state;
-     const { match: { url }, classes } = this.props;
-     return (
-       <>
-         <Box p={1} />
-         <div className={classes.button}>
-           <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-       Add Trainee
-           </Button>
-         </div>
-         <FormDialog open={open} onClose={this.handleClose} onSubmit={this.onSubmitHandle} />
-         <Box p={1} />
-         <TableComponent
+  handleSort = (value) => {
+    const { orderBy, order } = this.state;
+    const isAsc = orderBy === value && order === 'asc';
+    const data = isAsc ? 'desc' : 'asc';
+    this.setState({
+      order: data,
+      orderBy: value,
+    });
+  }
 
-           id="id"
+  handleSelectChange = (value) => {
+    console.log(value);
+  }
 
-           data={trainee}
 
-           column={[{
-             field: 'name',
-             label: 'Name',
-             align: 'center',
-           },
-           {
-             field: 'email',
-             label: 'Email-Address',
+  render() {
+    const { open, order, orderBy } = this.state;
+    const { match: { url }, classes } = this.props;
+    const getDateFormatted = (date) => moment(date).format('dddd,MMMM Do YYYY, h:mm:ss a');
+    return (
+      <>
+        <Box p={1} />
+        <div className={classes.button}>
+          <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+            Add Trainee
+          </Button>
+        </div>
+        <FormDialog open={open} onClose={this.handleClose} onSubmit={this.onSubmitHandle} />
+        <Box p={1} />
+        <TableComponent
 
-           }]}
+          id="id"
 
-         />
-         <Box p={1} />
-         <ul>
-           {trainee.length && trainee.map((data) => (
-             <Fragment key={data.id}>
-               <li>
-                 <Link to={`${url}/${data.id}`}>{data.name}</Link>
-               </li>
+          data={trainee}
 
-             </Fragment>
-           ))}
-         </ul>
-       </>
-     );
-   }
+          column={[{
+            field: 'name',
+            label: 'Name',
+          },
+          {
+            field: 'email',
+            label: 'Email-Address',
+
+          },
+          {
+            field: 'createdAt',
+            label: 'Date',
+            align: 'right',
+            format: getDateFormatted,
+          }]}
+
+          order={order}
+          orderBy={orderBy}
+          onSort={this.handleSort}
+          onSelect={this.handleSelectChange}
+
+        />
+        <Box p={1} />
+        <ul>
+          {trainee.length && trainee.map((data) => (
+            <Fragment key={data.id}>
+              <li>
+                <Link to={`${url}/${data.id}`}>{data.name}</Link>
+              </li>
+
+            </Fragment>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 export default withStyles(useStyles, { withTheme: true })(TraineeList);
+
+TraineeList.propTypes = {
+  match: propTypes.objectOf(propTypes.any).isRequired,
+  classes: propTypes.element.isRequired,
+};
--- src/pages/Trainee/components/Table/Table.jsx
+++ src/pages/Trainee/components/Table/Table.jsx
@@ -5,6 +5,7 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
@@ -17,18 +18,38 @@ const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 650,
     border: 'solid',
-    borderWidth:'thin',
-    borderColor:'lightGrey'
+    borderWidth: 'thin',
+    borderColor: 'lightGrey',
   },
   color: {
-    color: 'grey',
+    color: 'red',
   },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+//   tableCell: {
+//     color: 'red',
+//   },
+
 }));
 
 const TableComponent = (props) => {
   const classes = useStyles();
-
-  const { id, data, column } = props;
+  const [open, setOpen] = React.useState(false);
+  const {
+    id, data, column, order, orderBy, onSort, onSelect,
+  } = props;
+  const handleSortIcon = () => {
+    setOpen(true);
+  };
 
   return (
     <TableContainer component={Paper} className={classes.container}>
@@ -36,14 +57,31 @@ const TableComponent = (props) => {
         <TableHead>
           <TableRow key={id}>
             {column.length && column.map((col) => (
-              <TableCell align={col.align} className={classes.color}>{col.label}</TableCell>
+              <TableCell align={col.align} className={classes.color}>
+                <TableSortLabel
+                  
+                 
+                  className={classes.tableCell}
+                  active={orderBy === col.field}
+                  direction={orderBy === col.field ? order : 'asc'}
+                  onClick={() => onSort(col.field)}
+                  hideSortIcon={open}
+                >
+                  {col.label}
+                  {orderBy === col.field ? (
+                    <span className={classes.visuallyHidden}>
+                      {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                    </span>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
             ))}
 
           </TableRow>
         </TableHead>
         <TableBody>
           {data.length && data.map((element) => (
-            <TableRow key={element[id]}>
+            <TableRow hover key={element[id]}>
               {column.map(({ field, align }) => (
 
                 <TableCell align={align}>{element[field]}</TableCell>
